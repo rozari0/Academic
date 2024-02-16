14a1c7651bb599263571395d9e261c3e027d67d2
rozari0 <rozario@tuta.io>
2024-02-16 16:20:29 +0600

    follow question.

--- 13-02-2024/hw/allinone.c
+++ 13-02-2024/hw/allinone.c
@@ -11,18 +11,18 @@ int main() {
     printf("%d is odd.\n", num);
   }
 
-  if (num % 3 == 0 && num % 13 == 0) {
-    printf("%d is divisible by 3 and 13.\n", num);
-  } else {
-    printf("%d is not divisible by 3 and 13.\n", num);
-  }
-
   if (num > 0) {
     printf("%d is positive.\n", num);
   } else if (num < 0) {
     printf("%d is negetive.\n", num);
   } else {
     printf("This number is zero.\n");
+  }
+
+  if (num % 3 == 0 && num % 13 == 0) {
+    printf("%d is divisible by 3 and 13.\n", num);
+  } else {
+    printf("%d is not divisible by 3 and 13.\n", num);
   }
 
   return 0;
